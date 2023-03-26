70179218910e27d94b1ce267648d9fa1cc4b79ad
Fureshi <68948864+vantruongno99@users.noreply.github.com>
2023-03-26 13:46:21 +1100

    update

--- Subscriber/index.ts
+++ Subscriber/index.ts
@@ -46,7 +46,7 @@ client.subscribe('mqtt/test');
 
 setInterval(() => {
     const obj = {
-        SensorCode:'Test',
+        SensorCode: 'Test',
         TimeStamp: new Date(),
         Utc: Math.floor(Date.now() / 1000),
         SensorData: JSON.stringify({ Name: 'test' })
@@ -61,3 +61,48 @@ const publish = (topic: string, msg: string) => {
         client.publish(topic, msg);
     }
 }
+
+const typeofMessage = (message: String) => {
+    const type = message.split(',')[0]
+    if (type !== "CFG") {
+        return `${message.split(',')[0]},${message.split(',')[1]}`
+    }
+    return type
+}
+
+const logMessageHandle = (message: String, topic: String) => {
+    const value: String[] = message.split(',')
+    return
+    ({
+        TaskId: value[1],
+        msgTimeUTC: value[2],
+        logType: value[3],
+        logValue: value[4],
+        logNote: value[5],
+        SensorId: topic
+    })
+}
+const checkMessageHandle = (message: String, topic: String) => {
+    const value: String[] = message.split(',')
+    return
+    ({
+        TaskId: value[1],
+        msgTimeUTC: value[2],
+        SensorId: topic
+    })
+}
+
+const CalMessageHandle = (message: String, topic: String) => {
+    const value: String[] = message.split(',')
+    return
+    ({
+        Status: value[1],
+        msgTimeUTC: value[2],
+        gasType: value[3],
+        calType: value[4],
+        setValue: value[5],
+        measVal: value[6],
+        SensorId: topic
+    })
+}
+
